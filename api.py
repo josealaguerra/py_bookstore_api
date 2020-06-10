22a5f355f9ade6d5c6933838475df9a25f01da53
josealaguerra <josealaguerra@gmail.com>
2020-06-10 08:28:24 -0600

    Versión 3, se agregaron rutas especificas para filtrar

--- api.py
+++ api.py
@@ -30,9 +30,32 @@ def home():
 <p>A prototype API for distant reading of science fiction novels.</p>'''
 
 
-# A route to return all of the available entries in our catalog.
 @app.route('/api/v1/resources/books/all', methods=['GET'])
 def api_all():
     return jsonify(books)
+
+
+@app.route('/api/v1/resources/books', methods=['GET'])
+def api_id():
+    # Check if an ID was provided as part of the URL.
+    # If ID is provided, assign it to a variable.
+    # If no ID is provided, display an error in the browser.
+    if 'id' in request.args:
+        id = int(request.args['id'])
+    else:
+        return "Error: No id field provided. Please specify an id."
+
+    # Create an empty list for our results
+    results = []
+
+    # Loop through the data and match results that fit the requested ID.
+    # IDs are unique, but other fields might return many results
+    for book in books:
+        if book['id'] == id:
+            results.append(book)
+
+    # Use the jsonify function from Flask to convert our list of
+    # Python dictionaries to the JSON format.
+    return jsonify(results)
 
 app.run()
