3ae711ba6cddcd30e624415fac8f26a28d00ddd8
josealaguerra <josealaguerra@gmail.com>
2020-06-10 16:27:39 -0600

    Versión 4, agregue un batch para que se ejecute automaticamente y tambien de donde se obtuvo este codigo

---
+++ execute.bat
@@ -0,0 +1,11 @@
+
+start python api.py
+pause
+rem start google-chrome:http://127.0.0.1:5000/
+rem start google-chrome:http://127.0.0.1:5000/api/v1/resources/books/all
+
+
+start http://127.0.0.1:5000/
+start http://127.0.0.1:5000/api/v1/resources/books/all
+
+
